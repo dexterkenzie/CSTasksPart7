050d812dd4cae535be81e29b049522309817f0be
«dashkovsky» <>
2022-09-24 13:48:07 +0300

    task 47 done

--- task47/Program.cs
+++ task47/Program.cs
@@ -5,3 +5,37 @@
 // 0,5 7 -2 -0,2
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
+
+Console.WriteLine("Количество строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Количество столбцов:");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+double[,] numbersMatrix = new double[m, n];
+FillArrayRandomNumbers(numbersMatrix);
+PrintArray(numbersMatrix);
+
+void FillArrayRandomNumbers(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(-99, 100));
+        }
+    }
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j],4} "); // 4 - количество символов, которое занимает каждое число; выравнивание печати массива
+        }
+        Console.WriteLine("");
+    }
+}
+
+Console.WriteLine();
