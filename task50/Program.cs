c1e34fcc84ecd1132efa814a2b61669f759d55a9
«dashkovsky» <>
2022-09-24 16:14:42 +0300

    task 50 done

--- task50/Program.cs
+++ task50/Program.cs
@@ -7,3 +7,96 @@
 // 5 9 2 3
 // 8 4 2 4
 // 17 -> такого числа в массиве нет
+
+
+// Шаг 1. Задать массив.
+// Пользователь задает строки и столбцы. 
+// Массив наполняется случайными числами.
+
+// Console.WriteLine("Количество строк: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Количество столбцов:");
+// int n = Convert.ToInt32(Console.ReadLine());
+// double[,] numbersMatrix = new double[m, n];
+
+// Console.WriteLine();
+// Console.WriteLine("Задан массив: ");
+// Console.WriteLine();
+
+// FillArrayRandomNumbers(numbersMatrix);
+// PrintArray(numbersMatrix);
+
+// void FillArrayRandomNumbers(double[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = Convert.ToDouble(new Random().Next(-9, 10));
+//         }
+//     }
+// }
+
+// void PrintArray(double[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write($"{array[i, j],3} ");
+//         }
+//         Console.WriteLine("");
+//     }
+// }
+
+// Console.WriteLine();
+
+// Шаг 2. Решение задачи.
+// Программа принимает позиции элемента и возвращает значение этого элемента 
+// или же указание, что такого элемента нет.
+
+
+Console.WriteLine("Строка: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Столбец: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] numbers = new int[10, 10];
+FillArrayRandomNumbers(numbers);
+
+if (n > numbers.GetLength(0) || m > numbers.GetLength(1))
+{
+    Console.WriteLine("Такого элемента не существует. ");
+}
+else
+{
+    Console.WriteLine($"Значение элемента на {n} строке {m} столбца равно [{numbers[n - 1, m - 1]}]");
+}
+
+Console.WriteLine();
+PrintArray(numbers);
+Console.WriteLine();
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-100, 100) / 10;
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j],3} ");
+        }
+        Console.Write(" |");
+        Console.WriteLine("");
+    }
+}
