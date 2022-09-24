21e68984d7008aac7a685cb954327ae375e0df76
«dashkovsky» <>
2022-09-24 16:04:34 +0300

    task 52 done

--- task52/Program.cs
+++ task52/Program.cs
@@ -7,3 +7,58 @@
 // 5 9 2 3
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+Console.WriteLine("Количество строк:");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Количество столбцов:");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] numbers = new int[n, m];
+
+FillArrayRandomNumbers(numbers);
+
+Console.WriteLine();
+Console.WriteLine("Заданный массив:");
+
+PrintArray(numbers);
+
+Console.WriteLine();
+Console.WriteLine("Среднее арифмитическое каждого столбца: ");
+
+for (int j = 0; j < numbers.GetLength(1); j++)
+{
+    double avarage = 0;
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        avarage = (avarage + numbers[i, j]);
+    }
+    avarage = avarage / n;
+    Console.Write($"{Math.Round(avarage, 1)}; ");
+}
+
+Console.WriteLine();
+
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-99, 100);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j],5} ");
+        }
+        Console.WriteLine("");
+    }
+}
